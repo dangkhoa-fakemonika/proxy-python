daa6668fc0af8512f5c9cb9c0f0ca495fd0f6d2a
dangkhoa-fakemonika <mcfacebook911+github@gmail.com>
2023-08-13 13:47:50 +0700

    update on code

--- proxy_raw.py
+++ proxy_raw.py
@@ -20,38 +20,38 @@ def debugPrinting(whole_data):
     db_file.write("\nend of debugging one\n\n\n")
     db_file.close()
 
-def getData(hostName, method):
+def getInformation(whole_data):
+    try:
+        method = whole_data[0].split(" ")
+        host = whole_data[1].split(" ")
+
+    #return ''.join(format(ord(i), '08b') for i in method[0]), ''.join(format(ord(i), '08b') for i in host[1])
+    #print(method)
+    #print(host)
+        return method[0],host[1]
+    except:
+        return "EXIT", "EXIT"
+
+def useMethod():
+    print()
+def getData():
     #Set up the server
     s = socket(AF_INET,SOCK_STREAM)
-    s.connect((hostName,80))
-    method_str = b""
-    if (method == 0):
-        method_str = b"GET"
-    elif (method == 1):
-        method_str = b"HEAD"
-    elif (method == 2):
-        method_str = b"POST"
-
-    if (method_str == b""):
-        print("NOT SUPPORTED METHOD")
-        return
-
-    req_sequence = method_str + b" / HTTP/1.1\r\nHost:www.example.com\r\n\r\n"
-
-    #Sent request
-    sent_bytes = 0 #Counting bytes
-    while sent_bytes < len(req_sequence):
-        sent_bytes += s.send(req_sequence[sent_bytes:])
-
+    #s.connect((hostName,80))
+    s.bind(("127.0.0.1",8888))
+    s.listen(5)
+    print("Waiting...")
+    conn, address = s.accept()
+    print("Connected to ", address)
     #Receiving data
 
     data = b""
     buffer_size = 4096
-    process_time = 3
-    s.settimeout(process_time)
+    process_time = 5
+    conn.settimeout(process_time)
     try:
         while 1:
-            chunk = s.recv(buffer_size)
+            chunk = conn.recv(buffer_size)
             if len(chunk) == 0:
                 print("No content left")
                 break
@@ -62,9 +62,39 @@ def getData(hostName, method):
     true_data = data.decode("ISO-8859-1")
 
     whole = true_data.split("\r\n")
-    debugPrinting(whole)
+    #debugPrinting(whole)
     for i in whole:
         print (i)
+
+    """method_str, host_name = getInformation(whole)
+    req_sequence = method_str + " / HTTP/1.1\r\nHost:www." + host_name + "\r\n\r\n"
+    #print(req_sequence)
+    if (method_str == "EXIT"):
+        return
+    request = req_sequence.encode("ISO-8859-1")
+    # Sent request
+    sent_bytes = 0  # Counting bytes
+
+    data = b""
+    while sent_bytes < len(request):
+        sent_bytes += conn.send(request[sent_bytes:])
+    try:
+        while 1:
+            chunk = conn.recv(buffer_size)
+            if len(chunk) == 0:
+                print("No content left")
+                break
+            data += chunk
+    except:
+        print("Waited too long")
+    #data = s.recv(buffer_size)
+    true_data = data.decode("ISO-8859-1")
+
+    whole = true_data.split("\r\n")
+    #debugPrinting(whole)
+    for i in whole:
+        print (i)"""
+    conn.close()
     s.close()
 
-getData("example.com",0)
+getData()
