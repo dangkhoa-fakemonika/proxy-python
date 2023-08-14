ff6015700a1520c08e94debccbd032f1cdfa0651
KanCh <hagiabao1958@gmail.com>
2023-08-14 08:58:03 +0700

    add more changes

--- proxy_raw.py
+++ proxy_raw.py
@@ -8,6 +8,9 @@ from datetime import datetime, time
 import os
 import re
 
+buffer_size = 4096
+process_time = 5
+
 def debugPrinting(whole_data):
     try:
         db_file = open("ex.txt","x")
@@ -20,43 +23,37 @@ def debugPrinting(whole_data):
     db_file.write("\nend of debugging one\n\n\n")
     db_file.close()
 
-def getInformation(whole_data):
-    try:
-        method = whole_data[0].split(" ")
-        host = whole_data[1].split(" ")
-
-    #return ''.join(format(ord(i), '08b') for i in method[0]), ''.join(format(ord(i), '08b') for i in host[1])
-    #print(method)
-    #print(host)
-        return method[0],host[1]
-    except:
-        return "EXIT", "EXIT"
+def getRequest(method, domain):
+    if method == "GET":
+        return method + " / HTTP/1.1\r\nHost:" + domain + "\r\n\r\n"
+    if method == "POST":
+        return method + " /auth HTTP/1.1\r\n"
+    if method == "HEAD":
+        return method + " / HTTP/1.1\r\nHost:" + domain + "\r\nAccept: text/html\r\n"
 
 def methodProcessing(message):
-    valid_methods = ("GET","POST","HEAD")
     try:
         method = message.split()[0]
-        print(method)
         url = message.split()[1]
-        print(url)
-        domain = url.split()[4]
-        print(domain)
-        #method = request.split()[0]
-        if method not in valid_methods:
-            return #403
+        domain = message.split()[4]
+        url = url[url.find('://')+3:]
+        file_path = url[url.find('/'):] 
         #send_msg = request.split()[0] + " / " + request.split()[2] + "\r\nHost:" + domain_msg.split()[1] + "\r\n\r\n"
         #print(request,domain_msg,domain,method,send_msg)
     except:
         print("No data received")
         return
-
+    #Check valid method
+    if method not in ("GET","POST","HEAD"):
+            return #403
+    #whitelist
+    request = getRequest(method, domain)
     page = socket(AF_INET,SOCK_STREAM)
     data = b""
-    buffer_size = 4096
-    process_time = 5
     page.settimeout(process_time)
     page.connect((domain, 80))
-    page.sendall(message.encode("ISO-8859-1"))
+    print(request + message)
+    page.sendall((request + message).encode())
     try:
         while 1:
             chunk = page.recv(buffer_size)
@@ -70,29 +67,19 @@ def methodProcessing(message):
 
     #print(method)
     page.close()
-    true_data = data.decode()
-    print("Data received: ")
-    whole = true_data.split("\r\n")
+    #response = data.decode("ISO-8859-1")
+    #print("Data received: ")
+    #print(method)
+    #print(response)
+    #whole = true_data.split("\r\n")
     # debugPrinting(whole)
-    for i in whole:
-        print(i)
+    #for i in whole:
+    #    print(i)
     #return whole
 
-def connect(client, address):
+def connectionProcessing(client, address):
     #Receiving data
     data = b""
-    buffer_size = 4096
-    process_time = 5
-    """conn.settimeout(process_time)
-    try:
-        while 1:
-            chunk = conn.recv(buffer_size)
-            if len(chunk) == 0:
-                print("No content left")
-                break
-            data += chunk
-    except:
-        print("Waited too long")"""
     msg = client.recv(buffer_size)
     msg = msg[:-2]+b"Connection: Close\r\n\r\n"
     message = msg.decode("ISO-8859-1")
@@ -116,6 +103,6 @@ while True:
     print("Waiting...")
     clientSock, address = proxy.accept()
     print("Connected to ", address)
-    connect(clientSock,address)
+    connectionProcessing(clientSock,address)
 print("End")
 s.close()
