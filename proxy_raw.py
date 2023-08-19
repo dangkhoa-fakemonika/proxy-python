90900be8324d1e5c4305c6e6088301af812561c5
dangkhoa-fakemonika <mcfacebook911+github@gmail.com>
2023-08-19 20:35:09 +0700

    in yo walls i sugma L

--- proxy_raw.py
+++ proxy_raw.py
@@ -1,45 +1,52 @@
 #external libraries
 import socket
 from socket import *
-import sys
 import threading
-import time as pytime
-from datetime import date, datetime, time
+from datetime import date, datetime, timedelta, time
 import os
-import re
 import json
-
-buffer_size = 4096
-process_time = 5
 
 ##################################### CONFIG FILE READING #######################################
 def readJSONConfigFile(filename):
+    #Open file
     config_file = open(filename,"r")
     read_data = json.load(config_file)
 
-    content = ("cache_time","whitelisting_enabled","whitelist","time_restriction","time_allow","decode_format")
+    #Proxy main configurations
     cache_time = int(read_data["cache_time"])
+    buffer_size = int(read_data["buffer_size"])
+    process_time = int(read_data["process_time"])
+
+    #Whitelist configurations
     WLenabled = bool(read_data["whitelisting_enabled"])
     whitelist = read_data["whitelist"]
+
+    #Time restriction configurations
     restriction = read_data["time_restriction"]
     time_allow = read_data["time_allow"]
-    supported_img = read_data["supported_img_types"]
 
-    return cache_time,WLenabled,whitelist,restriction,time_allow,supported_img
+    #Decode
+    decoder = read_data["decoder"]
+
+    #Caching images configurations
+    supported_img = read_data["supported_img"]
+
+    return cache_time,WLenabled,whitelist,restriction,time_allow,supported_img,decoder,buffer_size,process_time
 
 #Read config file
-cache_time,WLenabled,whitelist,restriction,time_allow,supported_img = readJSONConfigFile("config.json")
+cache_time,WLenabled,whitelist,restriction,time_allow,supported_img,decoder,buffer_size,process_time = readJSONConfigFile("config.json")
 
 ######################################## REQUEST HANDLING ##########################################
 
 def getRequest(message,method,domain,url):
+    #Ensuring the right domain
     input_url = url
     if input_url.partition(domain)[2] == "/":
         url = input_url.partition(domain)[0] + input_url.partition(domain)[1]
     if input_url.partition("http://")[2].partition("/")[0] != domain:
         domain = input_url.partition("http://")[2].partition("/")[0]
 
-    #print("aaaaa: ",url)
+    #Formatting requests
     if method == "GET":
         return method + " " + url + " HTTP/1.1\r\nHost:" + domain + "\r\nConnection: close\r\n\r\n"
     if method == "HEAD":
@@ -54,41 +61,47 @@ def getRequest(message,method,domain,url):
         return request
 
 def return403(client_connect):
-    error_file = open("error403.html",'r')
-    error_text = error_file.read()
-    client_connect.send(b'HTTP/1.0 403 Forbidden\r\nContent-Type: text/html\r\n\r\n' + error_text.encode("ISO-8859-1"))
+
+    try:
+        error_file = open("error/error403.html",'r')
+        error_text = error_file.read()
+    except:
+        error_text = ''
+
+    client_connect.send(b'HTTP/1.0 403 Forbidden\r\nContent-Type: text/html\r\n\r\n' + error_text.encode(decoder))
 
     print("#################################")
     print("Process Terminated")
     print("#################################")
 
 ######################################### DATA PROCESSING #########################################################
-def writeReceiveData(message,method,domain,url):
+def receiveResponseData(message,method,domain,url):
+    #Get request format
     request = getRequest(message,method, domain, url)
-    page = socket(AF_INET, SOCK_STREAM)
     data = b""
-    print("Domain 1:", domain)
-    print("URL:", url)
     if url.partition("http://")[2].partition("/")[0] != domain:
         domain = url.partition("http://")[2].partition("/")[0]
-    print("Domain 2:", domain)
-    page.settimeout(process_time)
+
+    #Create server
+    page = socket(AF_INET, SOCK_STREAM)
+    page.settimeout(process_time) #Time out in case of reading
     page.connect((domain, 80))
 
-    page.sendall(request.encode("ISO-8859-1"))
+    #Send the request
+    page.sendall(request.encode(decoder))
+
     #Searching for Transfer-Encoding/Content-Length in header
     while 1:
         tmpbit = page.recv(1)
         data += tmpbit
         if b"\r\n\r\n" in data:
             break
-    print("Decode process: ")
-    print(data.decode("ISO-8859-1"))
-    print("End of decode")
-    if b"Transfer-Encoding: chunked" in data and 1:
+
+    #Check for Transfer-Encoding
+    if b"Transfer-Encoding: chunked" in data:
         while 1:
             try:
-                #Read the number
+                #Read the number of each block
                 number = b""
                 while 1:
                     tmpbit = page.recv(1)
@@ -98,13 +111,14 @@ def writeReceiveData(message,method,domain,url):
                 data += number
                 num = int(number.strip(b'\r\n'),16)
 
-                if num == 0:
+                #Read data block
+                if num == 0: #End of data
                     data += b"\r\n"
                     break
                 else:
                     num += 2
                     try:
-                        while 1:
+                        while 1: #Use chunking method to guarantee data receiving
                             chunk = page.recv(min(buffer_size, num))
                             if len(chunk) == 0:
                                 print("No content left")
@@ -117,11 +131,13 @@ def writeReceiveData(message,method,domain,url):
                         print("End of line data")
             except:
                 print("Waited too long")
-    elif b"Content-Length: " in data and 1:
-        num = int(data.decode("ISO-8859-1").partition("Content-Length: ")[2].partition("\r\n")[0])
+    #Check for Content-Length
+    elif b"Content-Length: " in data:
+        #Read the length
+        num = int(data.decode(decoder).partition("Content-Length: ")[2].partition("\r\n")[0])
         print("Length:",num)
         try:
-            while 1:
+            while 1: #Use chunking method to guarantee data receiving
                 chunk = page.recv(min(buffer_size,num))
                 if len(chunk) == 0:
                     print("No content left")
@@ -133,7 +149,7 @@ def writeReceiveData(message,method,domain,url):
         except:
             print("End of line data")
     else:
-        #Main process
+        #Subtitute process for rare cases
         try:
             while 1:
                 chunk = page.recv(buffer_size)
@@ -144,62 +160,80 @@ def writeReceiveData(message,method,domain,url):
         except:
             print("Waited too long")
 
+    #End communication from the web server
     page.close()
     return data
 ##################################################DATA HANDLING###############################################
 def saveCacheImages(response,domain,path,file_name):
-    cache_img_content = response.partition("\r\n\r\n")[2].encode("ISO-8859-1")
+
+    #Save cache content
+    cache_img_content = response.partition("\r\n\r\n")[2].encode(decoder)
     cache_img = open("cache/" + domain + "/" + path + "/" + file_name, "wb")
     cache_img.write(cache_img_content)
     cache_img.close()
-    cache_header_content = response.partition("\r\n\r\n")[0].encode("ISO-8859-1")
+
+    #Save cache header
+    cache_header_content = response.partition("\r\n\r\n")[0].encode(decoder)
     cache_img_header = open("cache/" + domain + "/" + path + "/" + file_name.partition(".")[0] + ".bin", "wb")
     cache_img_header.write(cache_header_content)
-    cache_img_header.write(b"\r\n\r\n" + datetime.now().strftime("%H/%M/%S/%d/%m/%y").encode("ISO-8859-1"))
+    cache_img_header.write(b"\r\n\r\n" + datetime.now().strftime("%H/%M/%S/%d/%m/%y").encode(decoder))
     cache_img_header.close()
 
 def createCacheData(message,method,domain,url):
-    if 0 or method != 'GET' or message.partition("\r\n\r\n")[0].find("Accept: image") == -1: #I dont like caching for other methods
-        data = writeReceiveData(message,method,domain,url)
-        response = data.decode("ISO-8859-1")
+    # Check for cache-able contents
+    if method != 'GET' or message.partition("\r\n\r\n")[0].find("Accept: image") == -1:
+        data = receiveResponseData(message,method,domain,url)
+        response = data.decode(decoder)
         print(response)
     else:
+        #Get file name and extension and folder path
         file_name = url.split("/").pop()
         extension = file_name.partition(".")[2]
         path = url.partition("http://" + domain)[2].partition("/" + file_name)[0]
 
+        #Check if the image is supported
         if extension not in supported_img:
-            data = writeReceiveData(message, method, domain, url)
-            response = data.decode("ISO-8859-1")
+            data = receiveResponseData(message, method, domain, url)
+            response = data.decode(decoder)
             print(response)
             return
 
         try:
-            # No folder for caching (no data cached)
+            #No folder for caching (no data cached)
             os.makedirs("cache/" + domain + "/" + path)
-            data = writeReceiveData(message,method,domain,url)
-            response = data.decode("ISO-8859-1")
+            data = receiveResponseData(message,method,domain,url)
+            response = data.decode(decoder)
+
             saveCacheImages(response,domain,path,file_name)
         except:
+            #Folder path found
+            #Check for the cache file validity
             try:
                 #Check for cached file existing
                 cache_img = open("cache/" + domain + "/" + path + "/" + file_name, "rb")
                 cache_img.close()
                 cache_img_header = open("cache/" + domain + "/" + path + "/" + file_name.partition(".")[0] + ".bin", "rb")
                 cache_img_header.close()
-                cache_header = cache_img_header.read().decode("ISO-8859-1")
+                cache_header = cache_img_header.read().decode(decoder)
+
+                #Get last cache time stamp
                 header,blank,timecheck = cache_header.partition("\r\n\r\n")
                 dd,mm,yy,hh,mm,ss = timecheck.split("/")
-                getdate = datetime(yy,mm,dd,hh,mm,ss) + datetime.timedelta(seconds=cache_time)
+                getdate = datetime(yy,mm,dd,hh,mm,ss) + timedelta(seconds=cache_time)
+
+                #Test for validity
                 if getdate > datetime.now():
                     raise TypeError() #Cancel cache to write new cache
 
-                response = header + "\r\n\r\n"+ cache_img.read().decode("ISO-8859-1")
-                data = response.encode("ISO-8859-1")
+                #Write valid cache and send back to client
+                response = header + "\r\n\r\n"+ cache_img.read().decode(decoder)
+                data = response.encode(decoder)
 
             except:
-                data = writeReceiveData(message,method,domain,url)
-                response = data.decode("ISO-8859-1")
+                #No data cached found or cache file expires
+                #Pushing the request to the web server
+                data = receiveResponseData(message,method,domain,url)
+                response = data.decode(decoder)
                 saveCacheImages(response,domain,path,file_name)
     return data
 
@@ -207,20 +241,20 @@ def createCacheData(message,method,domain,url):
 
 def methodProcessing(message,client):
     try:
+        #Get information
         method = message.split()[0]
         url = message.split()[1]
         domain = message.split()[4]
-        print("URL:",url)
     except:
         print("No data received")
         return
 
-    #Kiểm tra phương thức hợp lệ
+    #Test for valid methods
     if method not in ("GET","POST","HEAD"):
             return403(client)
             return #403
 
-    #Danh sách cho phép
+    #Test for whitelisted websites
     if WLenabled:
         if domain not in whitelist:
             print("Not available")
@@ -230,9 +264,11 @@ def methodProcessing(message,client):
     ##################### GO TO WRITE RECEIVE DATA IF GONE WRONG ########################
     data = createCacheData(message,method,domain,url)
     #####################################################################################
+    response = data.decode(decoder)
 
-    response = data.decode("ISO-8859-1")
+    #Send the data to the client
     client.send(data)
+
     print("Data received: ")
     whole = response.split("\r\n")
     for i in whole:
@@ -240,7 +276,9 @@ def methodProcessing(message,client):
             break
         print(i)
 
-def connectionProcessing(client, address):
+def connectionProcessing(client):
+
+    #Test for restricted access time
     if restriction:
         if (time(time_allow[0],0,0) < datetime.now().time() < time(time_allow[1],0,0)):
             print("Not available")
@@ -248,14 +286,13 @@ def connectionProcessing(client, address):
             return #403
 
     #Receiving data
-    data = b""
     msg = client.recv(buffer_size)
-    #msg = msg[:-2]+b"Connection: Close\r\n\r\n"
-    message = msg.decode("ISO-8859-1")
+    message = msg.decode(decoder)
 
     whole = message.split("\r\n")
     for i in whole:
         print(i)
+    #Process the request
     methodProcessing(message,client)
     client.close()
 
@@ -266,13 +303,16 @@ def connectionProcessing(client, address):
 
 ############################################## PROXY MAIN #################################################################
 
+#Initiate proxy server
 proxy = socket(AF_INET,SOCK_STREAM)
 proxy.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
 proxy.bind(("127.0.0.1",8888))
 proxy.listen(20)
 print("Currently listening...")
 
+#Proxy server processes
 while True:
+    #Accpeting and getting the address and port
     clientSock, address = proxy.accept()
     addr_name = str(address[0]) + ' : ' + str(address[1]) # 127.0.0.1 : XXXXX
 
@@ -281,7 +321,7 @@ while True:
     print("#################################")
 
     #Initiate threading
-    main_thread = threading.Thread(name=addr_name,target=connectionProcessing,args=(clientSock,addr_name))
+    main_thread = threading.Thread(name=addr_name,target=connectionProcessing,args=(clientSock,))
     main_thread.start()
 
     #main_thread.join()
